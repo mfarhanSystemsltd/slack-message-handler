6bc0bd52b90b34e3376fdd59c875b20d228dcfba
Gerald Nunn <gerald.b.nunn@gmail.com>
2022-08-19 14:55:08 -0700

    Fix various issues, add logging

--- src/main/java/com/redhat/demo/pipeline/PipelineService.java
+++ src/main/java/com/redhat/demo/pipeline/PipelineService.java
@@ -3,6 +3,7 @@ package com.redhat.demo.pipeline;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
@@ -12,5 +13,6 @@ public interface PipelineService {
     
     @POST
     @Consumes("application/json")
+    @Produces("application/json")
     String startPipeline(PushToProdPipeline model);
 }
